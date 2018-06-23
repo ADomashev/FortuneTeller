22f0bc20fd26de6c8a3461f91f0d4612182a7683
ADomashev <37477774+ADomashev@users.noreply.github.com>
2018-06-23 17:53:34 +0300

    Add files via upload

--- src/by/alex/itcourses/run/RunCollection.java
+++ src/by/alex/itcourses/run/RunCollection.java
@@ -1,6 +1,5 @@
 package by.alex.itcourses.run;
 
-
 import java.util.Set;
 
 import by.alex.itcourses.entity.Client;
@@ -12,26 +11,21 @@ public class RunCollection {
 	public static void main(String[] args) {
 		Predictor predictor = new Predictor();
 		predictor.loadPrediction();
-		
-//		predictor.displayPredAndAnswers();
-//		Client client = new Client("Serega");
-//		Client client2 = new Client("Client2");
+
 		for (int i = 0; i < 10; i++) {
-			//Client client3 = new Client("Client"+i);
-			predictor.getInLine(new Client("Client "+i));
+
+			predictor.getInLine(new Client("Client " + i));
 		}
-//		predictor.displayClients();
 		int countClient = 0;
-		while(countClient<= 100) {
+		while (countClient <= 100) {
 			Client client = predictor.nextClient();
 			Set<Prediction> setPrediction = predictor.getSetPrediction();
-			//System.out.println(setPrediction);
 			Prediction prediction = client.choosePrediction(setPrediction);
 			client.askPredictor(prediction, predictor);
 			predictor.addRandomClient();
 			System.out.println();
-			System.out.println(client.getNameClient() +" How much times: "+client.getCountHowMore());
-			
+			System.out.println(client.getNameClient() + " How much times: " + client.getCountHowMore());
+
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
@@ -39,25 +33,10 @@ public class RunCollection {
 			}
 			countClient++;
 		}
-			System.out.println();
-			System.out.println("WaitList");
-			predictor.displayWaitList();
-		
-//		
-//		Client client = predictor.nextClient();
-//		System.out.println(client);
-//		Set<Prediction> setPrediction = predictor.getSetPrediction();
-//		//System.out.println(setPrediction);
-//		Prediction prediction = client.choosePrediction(setPrediction);
-//		//System.out.println(prediction);
-//		client.askPredictor(prediction, predictor);
-//		
-//		Prediction prediction1 = client.choosePrediction(setPrediction);
-//		client.askPredictor(prediction1, predictor);
-//		System.out.println(client);
-//		System.out.println();
-//		System.out.println("WaitList");
-//		predictor.displayWaitList();
+		System.out.println();
+		System.out.println("WaitList");
+		predictor.displayWaitList();
+
 	}
 
 }
